08c10c913acd842f0bcc5f7393e6c4d482cfdae1
JeS24 <jyotirmaya.shivottam@niser.ac.in>
2024-05-27 14:50:13 +0530

    Reverted half-merged code

--- tirtha_bk/tirtha/urls.py
+++ tirtha_bk/tirtha/urls.py
@@ -16,8 +16,7 @@ pre = settings.PRE_URL
 urlpatterns = [
     path(pre + "competition/", views.competition, name="competition"),  # NOTE: Order matters
     path(pre + "howto/", views.howto, name="howto"),
-    path(pre + "signin-google/", views.tokenAuth, name="tokenAuth"),# NOTE: for the authlib and token #TODO:CHANGE THE NAME OF THE FUNCTION as PER GOOGLE BACKEND CONFIGS
-    path(pre + "signinGoogle/", views.google_login, name="signinGoogle"),  # NOTE: for the authlib and token #TODO:CHANGE THE NAME OF THE FUNCTION
+    path(pre + "googleAuth/", views.googleAuth, name="googleAuth"),
     path(pre + "logout/", views.logout, name="logout"),  # NOTE: For the logout of user
     path(pre + "preUpload/", views.pre_upload_check, name="preUpload"),
     path(pre + "upload/", views.upload, name="upload"),
@@ -28,6 +27,8 @@ urlpatterns = [
     # path(pre + "loadRun/", views.loadRun, name="loadRun"),
     path(pre + "models/<str:vid>/", views.index, name="indexMesh"),
     path(pre + "models/<str:vid>/<str:runid>/", views.index, name="indexMesh"),
-    re_path(rf"^{pre}(resolve/)?(?P<ark>ark:/?.*$)", views.resolveARK, name="resolveARK"), #LATE_EXP: Add support for `?info` and `??info` queries or something similar. Check ARK spec.
+    re_path(
+        rf"^{pre}(resolve/)?(?P<ark>ark:/?.*$)", views.resolveARK, name="resolveARK"
+    ),  # LATE_EXP: Add support for `?info` and `??info` queries or something similar. Check ARK spec.
     path(f"{pre}", views.index, name="index"),
 ]
